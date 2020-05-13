f562bc8cb38a654870123034141e3bd90ac03872
lchaconc <luis.chacon.campos@gmail.com>
2020-05-12 22:02:30 -0600

    Update AcercaDe.js
    Se agega info

--- src/comp/AcercaDe.js
+++ src/comp/AcercaDe.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import config from '../config.json';
+import alertify from 'alertifyjs';
+import 'alertifyjs/build/css/alertify.min.css';
+import 'alertifyjs/build/css/themes/default.min.css';
 
+
+
+const urlImg = config.assetsImg + "fotos/" ;
 
 function AcercaDe(props) {
+   
+
+    const handleAbrirImg =()=> {
+        alertify
+        .alert("Galería", 
+        '<img class="img-fluid" src="'+urlImg+'foto2.jpeg"  alt="foto 2" /> <hr>' + 
+        '<img class="img-fluid" src="'+urlImg+'foto3.jpeg"  alt="foto 2" />' ,
+         );
+
+    }
 
     return (
         <div className="container">
@@ -16,16 +33,45 @@ function AcercaDe(props) {
                 <i className="fas fa-home"></i> Inicio
             </button>
         </div>
-
-        <hr/>
+        
             <div className="row">
-                <div className="col-sm-12">
-                    <h4 className="text-info">ACERCA DE NOSOTROS:</h4>
+                <div className="col-sm-12 mt-1">
+                    <img 
+                        className="img-fluid" 
+                        src={urlImg + "foto1.jpeg" } 
+                        alt="Imagen encabezado" 
+                        onClick={handleAbrirImg}
+                        />
                 </div>
             </div>
-
-        <br/>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea porro repellat hic, quae corrupti minus quisquam amet id blanditiis molestiae sed perferendis obcaecati veritatis alias natus nihil explicabo beatae et.
+        <div className="row">
+            <div className="col-sm-12 mt-2">
+            <h6>Comité de vecinos Portón del Prado</h6>
+            <p className="text-justify">
+            <strong>S</strong>omos un equipo de trabajo, que busca conocernos como vecinos y realizar acciones 
+            para mejoras del lugar en el que vivimos.
+            Dentro de las actividades que realizamos están:  
+            </p>
+            <ul>
+                <li>Generar acciones para la comunicación entre vecinos y la presentación de ideas de mejora.</li>
+                <li>Recuperación y mejoramiento del parque recreativo.</li>
+                <li>Campañas de reciclaje en coordinación con la Municipalidad de Coronado, los segundos y cuartos miércoles del mes (el reciclaje se deja a la entrada del Parque antes de las 6:00 a.m.).</li>
+                <li>Actividades recreativas y familiares de integración comunitaria.</li>
+                <li>Coordinación con instituciones, recaudación de fondos, entre otras acciones.</li>
+            </ul>
+            <p className="text-justify">
+                Puede contactarnos al correo del Comité: <br/>
+                <a href="mailto:portondelprado@gmail.com"> portondelprado@gmail.com </a> <br/>
+                Facebook: <a href="https://www.facebook.com/pages/Porton-Del-Prado-Vazquez-De-Coronado/396134720445386" target="_blank" rel="noopener noreferrer">
+                    Urbanización Portón del Prado, Coronado 
+                </a>      <br/>
+                Grupo de WhatsApp: Urbanización Portón del Prado. <br/>   
+                <strong>Contáctenos por correo para incluirlo. En el mensaje adjunte su nombre, teléfono y número de casa.</strong>
+            </p>
+            </div>
+        </div>
+        
+        
         </div>
     );
     
